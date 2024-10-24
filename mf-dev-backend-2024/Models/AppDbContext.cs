1ae627ca2cc02322446baee69ba32728d02b8c06
YasminMaia <164058828+YasminMaia@users.noreply.github.com>
2024-10-24 18:35:17 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-backend-2024/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2024.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veículo> Veículos { get; set; }
+
+    }
+}
